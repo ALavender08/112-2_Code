b0e51872324de7c1117695fe7d05d9ea83a17653
ALavender08 <160372913+ALavender08@users.noreply.github.com>
2024-03-08 23:30:59 +0800

    k516 根號(Sqrt)
    新增解題步驟及我的小建議

--- k516.cpp
+++ k516.cpp
@@ -16,39 +16,39 @@ int main()
     char l[row][col];
     int nowcol;
 
-    //��l��
+    //初始化
     for (int r=0;r<row;r++)
     {
         for (int c=0;c<col;c++)
             l[r][c]=' ';
     }
 
-    //����
+    //長邊
     for (int i=0;i<long_line;i++)
     {
         l[0][col-i-1]='*';
     }
 
-    //�u��
+    //短邊
     for (int i=0;i<short_line;i++)
     {
         l[row-left_line][i]='*';
     }
 
-    //���׽u
+    //左斜線
     for (int i=1;row-left_line+i<row;i++)
     {
         l[row-left_line+i][short_line+i-1]='*';
         nowcol=short_line+i-1;
     }
 
-    //�k�׽u
+    //右斜線
     for (int i=1;i<=right_line-2;i++)
     {
         l[row-1-i][nowcol+i]='*';
     }
 
-    //��X
+    //輸出
     for (int r=0;r<row;r++)
     {
         for (int c=0;c<col;c++)
@@ -56,6 +56,16 @@ int main()
         if (r!=row-1)
             cout<<endl;
     }
+    return 0;
+
+    /*
+    解題思路:
+    將要輸出的東西放入字元陣列紀錄
+
+    其實這題沒什麼技巧，我的解題過程就是:
+    觀察=>寫程式=>輸出結果=>繼續觀察=>繼續補程式=>繼續輸出結果再觀察=>一直重複直到AC
+    建議註解不然像我寫到後面就會有點亂
+    */
 
 
 }
