b37bd65a50a8f5a62408c6502b1909e510559836
ALavender08 <160372913+ALavender08@users.noreply.github.com>
2024-03-11 17:03:11 +0800

    e807 降雨量統計(Rainfall statistics)

---
+++ e807.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float rain[7][4],day,d_big=0,time,t_big=0;
+    for (int i=0;i<7;i++)
+    {
+        for (int j=0;j<4;j++)
+            cin>>rain[i][j];
+    }
+
+    for (int i=0;i<7;i++)
+    {
+        float d=0;
+        for (int j=0;j<4;j++)
+            d+=rain[i][j];
+        if (d>d_big)
+            {
+                day=i+1;
+                d_big=d;
+            }
+
+
+    }
+
+
+
+    for (int i=0;i<4;i++)
+    {
+        float t=0;
+        for (int j=0;j<7;j++)
+            t+=rain[j][i];
+        if (t>t_big)
+            {
+                time=i;
+                t_big=t;
+            }
+
+
+    }
+
+
+    cout<<day<<endl;
+
+    if (time==0)
+        cout<<"morning";
+    else if (time==1)
+        cout<<"afternoon";
+    else if (time==2)
+        cout<<"night";
+    else
+        cout<<"early morning";
+    return 0;
+
+}
