2efbb19fe6d53a562de93e3610597c245bbb2bd3
ALavender08 <160372913+ALavender08@users.noreply.github.com>
2024-03-23 16:20:22 +0800

    n362 質數遊戲 (Primes) 自己寫函式版

---
+++ TOI/n362_質數遊戲 (Primes)-2.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n,x=2;
+    cin>>n;
+
+    int t=1;
+    while (n>t*t)
+    {
+        t++;
+    }
+
+    while (x<=t)
+    {
+        //cout<<"in"<<endl;
+        if (n%x!=0)
+        {
+            x++;
+            continue;
+        }
+
+        int y=n/x , chx=2, chy=2, tx=1 , ty=1; // ch? >> check ?
+
+        bool xp=false, yp=false;
+        //?p >> ? is prime
+
+        //x
+        while (x>tx*tx)
+        {
+            tx++;
+        }
+
+        //
+        //cout<<tx<<endl;
+
+        while (chx<=tx)
+        {
+            if (x%chx==0 && x!=2)
+            {
+                break;
+            }
+            else if (chx==tx)
+                xp=true;
+            chx++;
+        }
+
+
+        //y
+        while (y>ty*ty)
+        {
+                ty++;
+        }
+
+        //
+        //cout<<ty<<endl;
+
+        while (chy<=ty)
+        {
+
+            if (y%chy==0 && y!=2)
+            {
+                break;
+            }
+            else if (chy==ty)
+                yp=true;
+            chy++;
+        }
+
+
+        if (xp && yp)
+        {
+            cout<<x<<" "<<y;
+            return 0;
+        }
+        else
+            x++;
+    }
+
+    cout<<"0 0";
+    return 0;
+
+    /*
+    注意: 這題不能一次 while 到底，就算TOI 算你對 Zerojudge 也會給你TLE (沒錯就是在說我
+          檢查因數只要檢查到他的開根號就可以出來了，不然一定TLE
+    */
+}
