6fa477ee2c382bcdd645e69b036062fc0c10c74a
ALavender08 <160372913+ALavender08@users.noreply.github.com>
2024-04-10 16:19:02 +0800

    Create a157_練習網.cpp

---
+++ unfinished/a157_練習網.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib[31]={0,1}; //全域變數初始化後後面全部預設0
+
+int F(int n)
+{
+    if (n==0 || n==1)
+        return fib[n];
+    else 
+    {
+        
+    }
+        return F(n-1)+F(n-2);
+}
+
+int main()
+{
+    0
+}
