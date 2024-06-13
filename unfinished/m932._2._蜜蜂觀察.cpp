754aac3a4ca278c4c78d869b3c59cf5426842811
ALavender08 <160372913+ALavender08@users.noreply.github.com>
2024-06-13 17:29:40 +0800

    Update m932._2._蜜蜂觀察.cpp

--- unfinished/m932._2._蜜蜂觀察.cpp
+++ unfinished/m932._2._蜜蜂觀察.cpp
@@ -28,7 +28,7 @@ int main()
         cin>>step[i];
 
     //
-    /*
+
     for (int i=0;i<r;i++)
     {
         for (int j=0;j<c*r;j++)
@@ -38,8 +38,52 @@ int main()
         cout<<endl;
 
     }
-    */
+
+    // 0 右上；1 右；2 右下；3 左下；4 左；5 左上
+
+    map <int,bool> bee;
+    int x=r,y=0,type=0;
+    string route[s];
+    for (int i=0;i<s;i++)
+    {
+        int now_step = step[i];
+        if (now_step<=2)
+        {
+            if ((x+y+1)%r==c)
+            {
+                route[i]=l[x][y];
+                continue;
+            }
+            if (now_step==0)
+            {
+                if (x==0)
+                {
+                    route[i]=l[x][y];
+                    continue;
+                }
+                x--;
+                y+=(r-1);
+                route[i] = l[x][y];
+                if (!bee.count(l[x][y]))
+                {
+                    bee[l[x][y]]=true;
+                    type++;
+                }
+            }
+            if (now_step==1)
+            {
+                y+=(r-1);
+                route[i] = l[x][y];
+                if (!bee.count(l[x][y]))
+                {
+                    bee[l[x][y]]=true;
+                    type++;
+                }
+            }
+        }
+        
+    }
     
-    
+
 
 }
